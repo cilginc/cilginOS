dd8d78722ccf33d08d5feb237df63649c1170fb2
cilginc <104676201+cilginc@users.noreply.github.com>
2025-06-24 23:34:19 +0300

    chore: updated permissions

--- releng/generate_profile.sh
+++ releng/generate_profile.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Kullanım: ./generate_profiledef.sh /hedef/klasor
-
 target_dir="$1"
 
 if [[ ! -d "$target_dir" ]]; then
-    echo "Kullanım: $0 /path/to/dir"
+    echo "Usage: $0 /path/to/dir"
     exit 1
 fi
 
-# Tüm dosyaları tara ve 644 olmayanları bul
 find "$target_dir" -type f ! -perm 644 | while read -r file; do
-    # UID, GID ve permissionları al
+    # Get UID, GID and permissionları
     read uid gid perm < <(stat -c "%u %g %a" "$file")
     echo "[\"$file\"]=\"$uid:$gid:$perm\""
 done
